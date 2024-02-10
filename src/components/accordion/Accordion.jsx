8bfb4e3b44212ac772de2e0d3135dc9bc17786d6
irriz-jayy <jayfelix789@gmail.com>
2024-02-10 10:35:57 +0300

    added initial accordion

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
 
 // single selection accordion
 
 const Accordion = () => {
-  return <div>Accordion</div>;
+  const [selected, setSelected] = useState(null);
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {data.map((dataItem) => (
+          <div className="item">
+            <div className="title">
+              <h3>{dataItem.question}</h3>
+              <span>+</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Accordion;
