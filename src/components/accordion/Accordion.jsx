595c7963a32d1a5e4a89d7875a4ec87b86cc83ba
irriz-jayy <jayfelix789@gmail.com>
2024-02-10 10:28:12 +0300

    added dummy data

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+// single selection accordion
+
 const Accordion = () => {
   return <div>Accordion</div>;
 };
---
+++ src/components/accordion/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: 1,
+    question: " Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    answer:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione facilis molestiae, debitis quidem magnam repellendus quibusdam? Non, officia deserunt labore soluta minima totam neque, maxime reiciendis nobis maiores optio asperiores?",
+  },
+  {
+    id: 2,
+    question: " Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    answer:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione facilis molestiae, debitis quidem magnam repellendus quibusdam? Non, officia deserunt labore soluta minima totam neque, maxime reiciendis nobis maiores optio asperiores?",
+  },
+  {
+    id: 3,
+    question: " Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    answer:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione facilis molestiae, debitis quidem magnam repellendus quibusdam? Non, officia deserunt labore soluta minima totam neque, maxime reiciendis nobis maiores optio asperiores?",
+  },
+];
+
+export default data;
