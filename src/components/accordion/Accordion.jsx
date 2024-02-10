9645c2a0261a79b998f2c7db2890bd1b55310dc2
irriz-jayy <jayfelix789@gmail.com>
2024-02-10 10:54:03 +0300

    added answer showng capability

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -5,15 +5,29 @@ import data from "./data";
 
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+    console.log(getCurrentId);
+    setSelected(getCurrentId);
+  }
+
   return (
     <div className="wrapper">
       <div className="accordion">
         {data.map((dataItem) => (
           <div className="item">
-            <div className="title">
+            <div
+              onClick={() => handleSingleSelection(dataItem.id)}
+              className="title"
+            >
               <h3>{dataItem.question}</h3>
               <span>+</span>
             </div>
+            {selected === dataItem.id ? (
+              <div className="content">
+                <p>{dataItem.answer}</p>
+              </div>
+            ) : null}
           </div>
         ))}
       </div>
