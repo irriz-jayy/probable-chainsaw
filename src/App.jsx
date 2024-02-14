c400d56e1bcd04a887d3086495e2af52eb982fae
irriz-jayy <jayfelix789@gmail.com>
2024-02-14 10:11:05 +0300

    renamed app to allow constants for loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Accordion from "./components/accordion/Accordion";
-import Loading from "./components/loading/Loading";
+import LoadingScreen from "./components/loading/LoadingScreen";
 import Slider from "./components/slider/Slider";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Slider url={"https://picsum.photos/v2/list"} limit={"10"} />
       </div>
       <div className="flex  justify-center items-center border h-[80vh]">
-        <Loading />
+        <LoadingScreen />
       </div>
     </>
   );
