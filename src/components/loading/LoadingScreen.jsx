3aeb0616cde9c4d4b00580d4a15a7df669128b42
irriz-jayy <jayfelix789@gmail.com>
2024-02-14 10:24:40 +0300

    added initial toggle loader

--- src/components/loading/LoadingScreen.jsx
+++ src/components/loading/LoadingScreen.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { ClipLoader } from "react-spinners";
 
 const LoadingScreen = () => {
-    
+  const [loading, setLoading] = useState(true);
+  const [color, setColor] = useState("#000000");
 
-  return <div>Loading</div>;
+  return (
+    <div className="sweet-loading">
+      <button onClick={() => setLoading(!loading)}>Toggle Loader</button>
+      <input
+        value={color}
+        onChange={(input) => setColor(input.target.value)}
+        placeholder="Color of the loader"
+      />
+
+      <ClipLoader
+        color={color}
+        loading={loading}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
 };
 
 export default LoadingScreen;
