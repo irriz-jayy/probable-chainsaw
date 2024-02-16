084477940ef2efaba72c3dc93b74e78604fe4164
irriz-jayy <jayfelix789@gmail.com>
2024-02-16 12:25:06 +0300

    added typewriter

--- src/components/typewriter/Typewriter.jsx
+++ src/components/typewriter/Typewriter.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import TypewriterComponent from "typewriter-effect";
 
 const Typewriter = () => {
-  return <div>Typewriter</div>;
+  return (
+    <div>
+      <TypewriterComponent
+        options={{
+          strings: ["Hey im Jay your favorite cousin"], // Your text here
+          autoStart: true, // Start typing automatically
+          loop: true, // Repeat the typing animation
+          cursor: "", // Hide the cursor
+          typeSpeed: 70, // Typing speed in milliseconds
+          delay: 70, // Delay between each character in milliseconds
+        }}
+      />
+    </div>
+  );
 };
 
 export default Typewriter;
